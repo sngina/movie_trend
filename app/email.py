dcc7995c3338ceb53bcb0958eb6ff8e6f3f06ce1
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-15 23:35:49 +0300

    updated the manage.py

--- app/email.py
+++ app/email.py
@@ -1,18 +1,18 @@
-# from flask_mail import Message
-# from flask import render_template
-# from app import mail
-# from flask import Flask
-# from flask_mail import Mail
+from flask_mail import Message
+from flask import render_template
+from app import mail
+from flask import Flask
+from flask_mail import Mail
 
-# app = Flask(__name__)
-# mail = Mail(app)
+app = Flask(__name__)
+mail = Mail(app)
 
-# sender_email = '.....@gmail.com'
-# subject_pref = 'Title:'
+sender_email = '.....@gmail.com'
+subject_pref = 'Title:'
 
-# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-# app.config['MAIL_PORT'] = 587
-# app.config['MAIL_USERNAME'] = 'projectsmoringa@gmail.com'
-# app.config['MAIL_PASSWORD'] = 'psw'
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'projectsmoringa@gmail.com'
+app.config['MAIL_PASSWORD'] = 'psw'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
