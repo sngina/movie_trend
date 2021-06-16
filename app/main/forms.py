5a40a4679f18759e0d22615ab7a48fd16c2fb926
sngina <sngina707@gmail.com>
2021-06-16 12:20:04 +0300

    add a class Review Form

---
+++ app/main/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm # this will help us create a form.
+from wtforms import   StringField , TextAreaField ,SubmitField # creation of textfields
+from wtforms.validators  import Required # this one ensures that all the fields are filled
+
+
+class ReviewForm(FlaskForm):
+  title = StringField('Review title', validators=[Required()]) # 2 para 1st = label 2nd = list of validtors
+  review = TextAreaField('Movie review')
+  submit = SubmitField('Submit')
