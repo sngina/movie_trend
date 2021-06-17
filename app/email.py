192032833c7b0e8b8b39f81ea22f995555de661a
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-17 12:30:56 +0300

    updates done on most files

--- app/email.py
+++ app/email.py
@@ -1,18 +1,9 @@
 from flask_mail import Message
 from flask import render_template
-from app import mail
-from flask import Flask
-from flask_mail import Mail
-
-app = Flask(__name__)
-mail = Mail(app)
-
-sender_email = '.....@gmail.com'
-subject_pref = 'Title:'
-
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'projectsmoringa@gmail.com'
-app.config['MAIL_PASSWORD'] = 'psw'
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+from . import mail
+def mail_message(subject,template,to,**kwargs):
+    sender_email = "jumaallanie@gmail.com"
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body= render_template(template + ".txt",**kwargs)
+    email.html = render_template(template + ".html",**kwargs)
+    mail.send(email)
--- config.py
+++ config.py
@@ -16,11 +16,11 @@ class Config:
 
     
     #  email configurations
-    # MAIL_SERVER = 'smtp.gmail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
      # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
